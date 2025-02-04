1b0529c83855ae96e0b146a227ae60ff024a1cd9
Alba <albaalonsoglez@gmail.com>
2025-02-04 13:54:36 +0100

    Proyecto Tienda2025: estructura básica y comienzo gestión de pedidos

---
+++ ProyectoTienda2025/src/main/java/es/educastur/alba/proyectotienda2025/Pedido.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.educastur.alba.proyectotienda2025;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alu01d
+ */
+public class Pedido {
+    private String idPedido;
+    private Cliente clientePedido;
+    private LocalDate fechaPedido;
+    ArrayList<LineaPedido> CestaCompra;
+    
+    public Pedido(String idPedido, Cliente clientePedido, LocalDate fechaPedido, ArrayList<LineaPedido> CestaCompra) {
+        this.idPedido = idPedido;
+        this.clientePedido = clientePedido;
+        this.fechaPedido = fechaPedido;
+        this.CestaCompra = CestaCompra;
+}
+
+    public String getIdPedido() {
+        return idPedido;
+    }
+
+    public Cliente getClientePedido() {
+        return clientePedido;
+    }
+
+    public LocalDate getFechaPedido() {
+        return fechaPedido;
+    }
+
+    public ArrayList<LineaPedido> getCestaCompra() {
+        return CestaCompra;
+    }
+
+    public void setIdPedido(String idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public void setClientePedido(Cliente clientePedido) {
+        this.clientePedido = clientePedido;
+    }
+
+    public void setFechaPedido(LocalDate fechaPedido) {
+        this.fechaPedido = fechaPedido;
+    }
+
+    public void setCestaCompra(ArrayList<LineaPedido> CestaCompra) {
+        this.CestaCompra = CestaCompra;
+    }
+        
+    @Override
+    public String toString() {
+        return idPedido + "-" + clientePedido + "-" + fechaPedido + "-" + CestaCompra;
+    }
+}
