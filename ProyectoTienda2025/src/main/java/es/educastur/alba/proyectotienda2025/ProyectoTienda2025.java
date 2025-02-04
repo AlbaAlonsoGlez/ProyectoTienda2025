9481a49616fac5651a43610233292cd9f57add6d
Alba <albaalonsoglez@gmail.com>
2025-02-04 13:43:16 +0100

    Proyecto Tienda2025: estructura básica y comienzo gestión de pedidos

---
+++ ProyectoTienda2025/src/main/java/es/educastur/alba/proyectotienda2025/ProyectoTienda2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.educastur.alba.proyectotienda2025;
+
+/**
+ *
+ * @author alu01d
+ */
+public class ProyectoTienda2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
